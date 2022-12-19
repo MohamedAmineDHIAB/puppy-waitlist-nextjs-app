27185325990eaab0b9391d5ec02bb65be730223d
Mohamed Amine DHIAB <aminedhiab2028@gmail.com>
2022-12-19 23:11:56 +0100

    added order based on prev and next entry

--- middleware/get.ts
+++ middleware/get.ts
@@ -9,6 +9,20 @@ export default function get(
     let copyFakeData: any[];
     const date = DateValue?.format("YYYY-MM-DD");
     copyFakeData = date ? Data.filter((item) => item.date === date) : Data;
+    // reorder every entries array in the data based on prevEntryId and nextEntryId
+    copyFakeData = copyFakeData.map((item: any) => {
+        let entries: any[];
+        entries = item.entries;
+        let newEntries: any[] = [];
+        let entry = entries.find((entry: any) => entry.prevEntryId === null);
+        while (entry) {
+            newEntries.push(entry);
+            entry = entries.find(
+                (entry: any) => entry.prevEntryId === newEntries.slice(-1)[0].id
+            );
+        }
+        return { ...item, entries: newEntries };
+    });
     copyFakeData = copyFakeData.map((item: any) => {
         let entries: any[];
         entries = item.entries;
